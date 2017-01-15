f70ca694d6b254226793317a762e97e6c8c18dcc
lamminghoi <lamminghoi@gmail.com>
2017-01-14 20:13:20 -0800

    Upload login screen files

--- app/src/main/java/edu/uw/tcss450/nutrack/Main.java
+++ app/src/main/java/edu/uw/tcss450/nutrack/Main.java
@@ -1,14 +1,13 @@
 package edu.uw.tcss450.nutrack;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //test1 test 2
 }
