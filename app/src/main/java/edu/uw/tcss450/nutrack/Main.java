abffe925c8d05a6bf7a0aa2235aa0899ea149f6f
lamminghoi <lamminghoi@gmail.com>
2017-01-17 19:16:01 -0800

    Add "Uncompleted" drawer bar

--- app/src/main/java/edu/uw/tcss450/nutrack/Main.java
+++ app/src/main/java/edu/uw/tcss450/nutrack/Main.java
@@ -1,20 +1,114 @@
 package edu.uw.tcss450.nutrack;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class Main extends AppCompatActivity {
+
+    private DrawerLayout myDrawer;
+
+    private Toolbar myToolbar;
+
+    private NavigationView myNaviDrawer;
+
+    private ActionBarDrawerToggle myDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar titleBar = (Toolbar) findViewById(R.id.titleBar);
-        setSupportActionBar(titleBar);
+        myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        myNaviDrawer = (NavigationView) findViewById(R.id.naviView);
+        myDrawer = (DrawerLayout) findViewById(R.id.main_frame);
 
+        setSupportActionBar(myToolbar);
+
+        myDrawerToggle = setupDrawerToggle();
+
+        myDrawer.addDrawerListener(myDrawerToggle);
+
+
+        initializeDrawerContent();
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        myDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                myDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void initializeDrawerContent() {
+        myNaviDrawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectDrawerItem(item);
+                return true;
+            }
+        });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_profile:
+                fragmentClass = Main.class;
+                break;
+            case R.id.nav_home:
+                fragmentClass = Main.class;
+                break;
+
+            default:
+                fragmentClass = Main.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        myDrawer.closeDrawers();
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, myDrawer, myToolbar, R.string.drawer_open, R.string.drawer_close);
+    }
 }
+
