1a5234175e3fbbac6462be39b906cdb2632de0a1
Ming Hoi Lam <lamminghoi@gmail.com>
2017-01-27 13:32:10 -0800

    Now the app is able to get data from api

--- app/src/main/java/edu/uw/tcss450/nutrack/API/FatSecretAPI.java
+++ app/src/main/java/edu/uw/tcss450/nutrack/API/FatSecretAPI.java
@@ -1,25 +1,60 @@
 package edu.uw.tcss450.nutrack.API;
 
+import android.net.Uri;
+import android.util.Base64;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
-/**
- * Created by Ming on 1/27/2017.
- */
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class FatSecretAPI {
 
-    private final String CONSUMER_KEY;
+    private final String METHOD = "GET";
 
-    private final String SECRET_KEY;
+    private final String CONSUMER_KEY = "";
+
+    private final String SECRET_KEY = "";
 
     private final String URL = "http://platform.fatsecret.com/rest/server.api";
 
     private final String SIGNATURE_METHOD = "HMAC-SHA1";
 
-    public FatSecretAPI(String theConsumerKey, String theSecretKey) {
-        CONSUMER_KEY = theConsumerKey;
-        SECRET_KEY = theSecretKey;
+    private final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+
+    public JSONObject searchFood(String searchFood) {
+        List<String> params = new ArrayList<>(Arrays.asList(generateOauthParams()));
+        String[] template = new String[1];
+        params.add("method=foods.search");
+        params.add("search_expression=" + Uri.encode(searchFood));
+        params.add("oauth_signature=" + sign(METHOD, URL, params.toArray(template)));
+
+        JSONObject foods = null;
+        try {
+            URL url = new URL(URL + "?" + paramify(params.toArray(template)));
+            URLConnection api = url.openConnection();
+            String line;
+            StringBuilder builder = new StringBuilder();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(api.getInputStream()));
+            while ((line = reader.readLine()) != null) builder.append(line);
+            JSONObject food = new JSONObject(builder.toString());   // { first
+            foods = food.getJSONObject("foods");                    // { second
+        } catch (Exception exception) {
+            Log.e("FatSecret Error", exception.toString());
+            exception.printStackTrace();
+        }
+        return foods;
     }
 
     public String generateRandomNonce() {
@@ -29,6 +64,24 @@ public class FatSecretAPI {
             n.append(r.nextInt(26) + 'a');
         }
         return n.toString();
+    }
+
+    private String sign(String method, String uri, String[] params) {
+        String[] p = {method, Uri.encode(uri), Uri.encode(paramify(params))};
+        String s = join(p, "&");
+        String tempSK = SECRET_KEY + "&";
+        SecretKey sk = new SecretKeySpec(tempSK.getBytes(), HMAC_SHA1_ALGORITHM);
+        try {
+            Mac m = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+            m.init(sk);
+            return Uri.encode(new String(Base64.encode(m.doFinal(s.getBytes()), Base64.DEFAULT)).trim());
+        } catch (java.security.NoSuchAlgorithmException e) {
+            Log.w("FatSecret_TEST FAIL", e.getMessage());
+            return null;
+        } catch (java.security.InvalidKeyException e) {
+            Log.w("FatSecret_TEST FAIL", e.getMessage());
+            return null;
+        }
     }
 
     public String[] generateOauthParams() {
@@ -44,7 +97,7 @@ public class FatSecretAPI {
         return baseString;
     }
 
-    private String paraify(String[] theParam) {
+    private String paramify(String[] theParam) {
         String[] param = Arrays.copyOf(theParam, theParam.length);
         Arrays.sort(param);
         return join(param, "&");
--- app/src/main/java/edu/uw/tcss450/nutrack/fragment/CaloriesCalculator.java
+++ app/src/main/java/edu/uw/tcss450/nutrack/fragment/CaloriesCalculator.java
@@ -2,12 +2,18 @@ package edu.uw.tcss450.nutrack.fragment;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.uw.tcss450.nutrack.API.FatSecretAPI;
 import edu.uw.tcss450.nutrack.R;
 
 
@@ -66,8 +72,54 @@ public class CaloriesCalculator extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calories_calculator, container, false);
+        View view = inflater.inflate(R.layout.fragment_calories_calculator, container, false);;
+
+        searchFood("apple");
+
+        return view;
+    }
+
+    private void searchFood(final String theFood) {
+        final FatSecretAPI mFatSecret = new FatSecretAPI();
+         new AsyncTask<String, String, String>() {
+             @Override
+             protected String doInBackground(String... arg0) {
+                 System.out.println("Starting to SearchFood");
+
+                 JSONObject food = mFatSecret.searchFood(theFood);
+                 JSONArray FOODS_ARRAY;
+                 try {
+                     if (food != null) {
+                         FOODS_ARRAY = food.getJSONArray("food");
+                         if (FOODS_ARRAY != null) {
+                             for (int i = 0; i < FOODS_ARRAY.length(); i++) {
+                                 JSONObject food_items = FOODS_ARRAY.optJSONObject(i);
+                                 String food_name = food_items.getString("food_name");
+                                 String food_description = food_items.getString("food_description");
+                                 String[] row = food_description.split("-");
+                                 String id = food_items.getString("food_type");
+                                 /*
+                                 if (id.equals("Brand")) {
+                                     brand = food_items.getString("brand_name");
+                                 }
+                                 if (id.equals("Generic")) {
+                                     brand = "Generic";
+                                 }
+                                 */
+                                 String food_id = food_items.getString("food_id");
+                                // mItem.add(new Item(food_name, row[1].substring(1),
+                                 //        "" + brand, food_id));
+                                 System.out.println(food_name);
+                                 System.out.println(food_id);
+                             }
+                         }
+                     }
+                 } catch (JSONException exception) {
+                     return "Error";
+                 }
+                 return "";
+             }
+         }.execute();
     }
 
 
